4c82b281042a028f7946a83aeb59a2dd945b406c
Chris LaRose <cjlarose@gmail.com>
2025-12-29 00:29:04 -0800

    Fix animal plushie trades

--- Common/src/main/java/io/github/sirjain0/perfectplushies/entity/WanderingPlushieTrader.java
+++ Common/src/main/java/io/github/sirjain0/perfectplushies/entity/WanderingPlushieTrader.java
@@ -16,30 +16,32 @@ import software.bernie.geckolib.core.animation.AnimatableManager;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class WanderingPlushieTrader extends WanderingTrader implements GeoEntity {
-    public static final Int2ObjectMap<VillagerTrades.ItemListing[]> TRADES = toIntMap(
-            ImmutableMap.of(1, BlockInit.plushieBlocks.stream().map(blockRegistryObject -> new VillagerTrades.ItemsForEmeralds(blockRegistryObject.get(), 5, 1, 1, 1)).toArray(VillagerTrades.ItemListing[]::new),
-                    2, BlockInit.playerBlocksCommon.stream().map(blockRegistryObject -> new VillagerTrades.ItemsForEmeralds(blockRegistryObject.get(), 10, 1, 1, 1)).toArray(VillagerTrades.ItemListing[]::new),
-                    3, BlockInit.playerBlocksRare.stream().map(blockRegistryObject -> new VillagerTrades.ItemsForEmeralds(blockRegistryObject.get(), 15, 1, 1, 1)).toArray(VillagerTrades.ItemListing[]::new),
-                    4, BlockInit.playerBlocksEpic.stream().map(blockRegistryObject -> new VillagerTrades.ItemsForEmeralds(blockRegistryObject.get(), 20, 1, 1, 1)).toArray(VillagerTrades.ItemListing[]::new)
-            )
-    );
     private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
     public WanderingPlushieTrader(EntityType<? extends WanderingTrader> $$0, Level $$1) {
         super($$0, $$1);
     }
 
-    private static Int2ObjectMap<VillagerTrades.ItemListing[]> toIntMap(ImmutableMap<Integer, VillagerTrades.ItemListing[]> pMap) {
+    private Int2ObjectMap<VillagerTrades.ItemListing[]> getTrades() {
+        return toIntMap(ImmutableMap.of(
+                1, BlockInit.plushieBlocks.stream().map(blockRegistryObject -> new VillagerTrades.ItemsForEmeralds(blockRegistryObject.get(), 5, 1, 1, 1)).toArray(VillagerTrades.ItemListing[]::new),
+                2, BlockInit.playerBlocksCommon.stream().map(blockRegistryObject -> new VillagerTrades.ItemsForEmeralds(blockRegistryObject.get(), 10, 1, 1, 1)).toArray(VillagerTrades.ItemListing[]::new),
+                3, BlockInit.playerBlocksRare.stream().map(blockRegistryObject -> new VillagerTrades.ItemsForEmeralds(blockRegistryObject.get(), 15, 1, 1, 1)).toArray(VillagerTrades.ItemListing[]::new),
+                4, BlockInit.playerBlocksEpic.stream().map(blockRegistryObject -> new VillagerTrades.ItemsForEmeralds(blockRegistryObject.get(), 20, 1, 1, 1)).toArray(VillagerTrades.ItemListing[]::new)
+        ));
+    }
 
+    private static Int2ObjectMap<VillagerTrades.ItemListing[]> toIntMap(ImmutableMap<Integer, VillagerTrades.ItemListing[]> pMap) {
         return new Int2ObjectOpenHashMap<>(pMap);
     }
 
     @Override
     protected void updateTrades() {
-        VillagerTrades.ItemListing[] commonTrades = TRADES.get(1);
-        VillagerTrades.ItemListing[] playerPlushieTrades = TRADES.get(2);
-        VillagerTrades.ItemListing[] playerPlushieTradesRare = TRADES.get(3);
-        VillagerTrades.ItemListing[] playerPlushieTradesEpic = TRADES.get(4);
+        Int2ObjectMap<VillagerTrades.ItemListing[]> trades = getTrades();
+        VillagerTrades.ItemListing[] commonTrades = trades.get(1);
+        VillagerTrades.ItemListing[] playerPlushieTrades = trades.get(2);
+        VillagerTrades.ItemListing[] playerPlushieTradesRare = trades.get(3);
+        VillagerTrades.ItemListing[] playerPlushieTradesEpic = trades.get(4);
         if (commonTrades != null && playerPlushieTrades != null && playerPlushieTradesRare != null && playerPlushieTradesEpic != null) {
             MerchantOffers offers = this.getOffers();
             this.addOffersFromItemListings(offers, commonTrades, 5);
